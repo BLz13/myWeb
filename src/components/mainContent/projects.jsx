538bb4b7fb9f5a576e025fff15576b9efab80a2e
Blz.13 <bruno.lazo@outlook.com>
2026-09-03 13:30:14 -0300

    version 5.6.2

--- src/components/mainContent/projects.jsx
+++ src/components/mainContent/projects.jsx
@@ -19,22 +19,6 @@ export default function Projects({ data, lang, sectionName }) {
 
     const [expandedProject, setExpandedProject] = useState(null);
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
-    
-    useEffect(() => {
-
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 1024);
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-
-    }, []);
-
     const toggleExpanded = (key) => {
         setExpandedProject((prev) =>
             prev === key ? null : key
