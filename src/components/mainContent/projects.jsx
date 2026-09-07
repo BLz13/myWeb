75f5626923ea3e7edc90b4630dc8b998dcfec543
Blz.13 <bruno.lazo@outlook.com>
2026-09-07 11:34:13 -0300

    version 5.6.3

--- src/components/mainContent/projects.jsx
+++ src/components/mainContent/projects.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 
 import CrossIcon from "../../assets/svg/cross.svg?react"
 import Gallery from "../gallery/gallery";
